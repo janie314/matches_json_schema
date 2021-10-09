348b1b03157d4712c2a68849db708d4a484d6f86
Jane <jane.d.340@protonmail.com>
2021-10-09 14:35:14 -0500

    1633808114

---
+++ examples/test.js
@@ -0,0 +1,37 @@
+import {
+  matches_json_schema,
+  matches_obj_schema,
+} from "../matches_json_schema.js";
+
+let schema_json = `
+{
+	"a": 0,
+	"b": {
+		"c": "",
+		"d": 0,
+		"e": null
+	}
+}
+`;
+
+// Matching Object
+let obj = {
+  a: 34,
+  b: {
+    c: "hello",
+    d: 12,
+    e: null,
+    f: [],
+  },
+  c: [undefined, 4],
+};
+
+console.log(matches_json_schema(schema_json, obj)); // Prints true
+
+// Schema
+let schema = [0];
+
+// Matching Object
+obj = [2, 3, 5, 7, 11, 13];
+
+console.log(matches_obj_schema(schema, obj)); // Prints true
---
+++ matches_json_schema.js
@@ -0,0 +1,55 @@
+// matches_json_schema
+// Version 1.0 (October 2021)
+// https://gitlab.com/jane314/matches_json_schema
+
+function type_name(x) {
+  const type_name = typeof (x);
+  if (
+    type_name === "string" || type_name === "number" ||
+    type_name === "undefined"
+  ) {
+    return type_name;
+  }
+  if (x === null) {
+    return "null";
+  }
+  if (Array.isArray(x)) {
+    return "Array";
+  }
+  return "Object";
+}
+
+export function matches_json_schema(schema_json, obj) {
+  try {
+    const schema = JSON.parse(schema_json);
+    return matches_obj_schema(schema, obj);
+  } catch (e) {
+    console.error(e.message);
+    return false;
+  }
+}
+
+export function matches_obj_schema(schema, obj) {
+  const schema_type = type_name(schema);
+  const obj_type = type_name(obj);
+  if (schema_type !== obj_type) {
+    return false;
+  }
+  // At this point we can assume schema_type === obj_type, and either they are both "Array" or both "Object"
+  if (schema_type === "Array") {
+    return obj.map((entry) => matches_obj_schema(schema[0], entry)).reduce(
+      (a, b) => a && b,
+      true,
+    );
+  }
+  // At this point we can assume schema_type === obj_type, and either they are both "Object"
+  for (const key in schema) {
+    if (!obj.hasOwnProperty(key)) {
+      return false;
+    }
+    if (!matches_obj_schema(schema[key], obj[key])) {
+      return false;
+    }
+  }
+  return true;
+}
